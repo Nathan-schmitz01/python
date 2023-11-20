e0141d67278277013b5b9fcb344d53a870ab6926
Nathan-schmitz01 <111901986+Nathan-schmitz01@users.noreply.github.com>
2023-11-19 18:40:28 -0600

    fixed problems with first submission

--- television.py
+++ television.py
@@ -7,72 +7,72 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         '''
         Method to set default values for the Television object.
         '''
         self.__status = False
         self.__muted = False
-        self.__volume = self.MIN_VOLUME
-        self.__channel = self.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
     
-    def power(self):
+    def power(self) -> None:
         '''
         Method to change the status instance variable of the Television object.
         '''
         self.__status = not self.__status
     
-    def mute(self):
+    def mute(self) -> None:
         '''
         Method to change the value of muted instance variable.
         '''
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         '''
         Method to increment the channel instance variable by one.
         '''
         if not self.__status:
             pass
 
-        elif self.__channel == self.MAX_CHANNEL:
-            self.__channel = self.MIN_CHANNEL
+        elif self.__channel == Television.MAX_CHANNEL:
+            self.__channel = Television.MIN_CHANNEL
         
         else:
             self.__channel += 1
     
-    def channel_down(self):
+    def channel_down(self) -> None:
         '''
         Method to decrement the channel instance variable by one.
         '''
         if not self.__status:
             pass
 
-        elif self.__channel == self.MIN_CHANNEL:
-            self.__channel = self.MAX_CHANNEL
+        elif self.__channel == Television.MIN_CHANNEL:
+            self.__channel = Television.MAX_CHANNEL
         
         else:
             self.__channel -= 1
     
-    def volume_up(self):
+    def volume_up(self) -> None:
         '''
         Method to increment the volume instance variable by one.
         '''
         if self.__status:
             self.__muted = False
 
-            if self.__volume != self.MAX_VOLUME:
+            if self.__volume != Television.MAX_VOLUME:
                 self.__volume += 1
     
-    def volume_down(self):
+    def volume_down(self) -> None:
         '''
         Method to decrement the volume instance variable by one.
         '''
         if self.__status:
             self.__muted = False
 
-            if self.__volume != self.MIN_VOLUME:
+            if self.__volume != Television.MIN_VOLUME:
                 self.__volume -= 1
     
     def __str__(self) -> str:
@@ -81,6 +81,6 @@ class Television:
         :return: a string with the current power, channel, and volume of the instance of the Television.
         '''
         if self.__muted:
-            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.MIN_VOLUME}"
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}"
         else:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
